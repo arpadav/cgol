81cc71c1e0bed3c9baccf5475eb0cfd245b6d0a4
unknown <arpadav@gmail.com>
2019-03-26 11:14:35 -0400

    removed commented out code on old

--- cgolold.js
+++ cgolold.js
@@ -129,37 +129,3 @@ function setFill(alive){
   if(alive){ ctx.fillStyle = "rgba(" + aliveRGB[0] + "," + aliveRGB[1] + "," + aliveRGB[2] + "," + (aliveRGB[3] / 255) + ")"; }
   else { ctx.fillStyle = "rgba(" + deadRGB[0] + "," + deadRGB[1] + "," + deadRGB[2] + "," + (deadRGB[3] / 255) + ")"; }
 }
-
-// drawGrid(canvasSize[0], canvasSize[1], "cgol");
-
-// function drawGrid(w, h, id) {
-//     var canvas = document.getElementById(id);
-//     var ctx = canvas.getContext('2d');
-//     ctx.canvas.width  = w;
-//     ctx.canvas.height = h;
-//
-//     var data = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"> \
-//         <defs> \
-//             <pattern id="smallGrid" width=String(w/resolution) height="8" patternUnits="userSpaceOnUse"> \
-//                 <path d="M 8 0 L 0 0 0 8" fill="none" stroke="black" stroke-width="0.5" /> \
-//             </pattern> \
-//             <pattern id="grid" width="80" height="80" patternUnits="userSpaceOnUse"> \
-//                 <rect width="80" height="80" fill="url(#smallGrid)" /> \
-//                 <path d="M 80 0 L 0 0 0 80" fill="none" stroke="black" stroke-width="1" /> \
-//             </pattern> \
-//         </defs> \
-//         <rect width="100%" height="100%" fill="url(#smallGrid)" /> \
-//     </svg>';
-//
-//     var DOMURL = window.URL || window.webkitURL || window;
-//
-//     var img = new Image();
-//     var svg = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
-//     var url = DOMURL.createObjectURL(svg);
-//
-//     img.onload = function () {
-//       ctx.drawImage(img, 0, 0);
-//       DOMURL.revokeObjectURL(url);
-//     }
-//     img.src = url;
-// }
